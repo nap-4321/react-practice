2d2c8ac70142f0a470eca0fe874bbf92e574eb9d
Naiti Patel <naitipatel63@gmail.com>
2024-07-07 12:12:56 -0400

    Dummy Commit

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -1,26 +1,26 @@
 
 const NameList = () => {
     const names =['Bruce', 'Wayne', 'Diana', 'Bruce']
-//     const persons =[{
-//         id: 1,
-//         name: 'Bruce',
-//         age: 30,
-//         skill: 'React' 
-//     },
-//     {
-//         id: 2,
-//         name: 'Wayne',
-//         age: 32,
-//         skill: 'Vue' 
-//     },
-//     {
-//         id: 3,
-//         name: 'Diana',
-//         age: 28,
-//         skill: 'Angular'  
-//     }
+    const persons =[{
+        id: 1,
+        name: 'Bruce',
+        age: 30,
+        skill: 'React' 
+    },
+    {
+        id: 2,
+        name: 'Wayne',
+        age: 32,
+        skill: 'Vue' 
+    },
+    {
+        id: 3,
+        name: 'Diana',
+        age: 28,
+        skill: 'Angular'  
+    }
 
-// ]
+]
 const namesList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
 return <div>{namesList}</div> 
 }
