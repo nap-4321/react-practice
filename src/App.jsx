2f871a6ec0d98645cb3aaa17247c58bb22b13d0c
p-naiti <naitipatel63@gmail.com>
2024-07-11 09:59:06 -0400

    codevolution-form

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,13 @@ import Inline from "./components/Inline";
 import "./appStyles.css";
 import styles from "./appStyles.module.css";
 import Form from "./components/Form";
+import FormThis from "./components/FormThis";
 
 function App() {
   return (
     <>
-      <Form />
+    {/* <FormThis/> */}
+      {/* <Form /> */}
       {/* <h1 className="error">Error</h1>
       <h1 className={styles.success}>Success</h1> */}
       {/* <Stylesheet primary={true} /> */}
---
+++ src/components/FormThis.jsx
@@ -0,0 +1,65 @@
+import React, {Component} from "react"
+
+class FormThis extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state ={
+            username: '',
+            comments: '',
+            topic: 'react'
+        }
+    }
+
+    handleUsernameChange = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    handleCommentsChange = (e) => {
+        this.setState({
+            comments: e.target.value
+        })
+    }
+
+    handleTopicChange = (e) => {
+        this.setState({
+            topic: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        alert(`${this.state.username}  ${this.state.comments}  ${this.state.topic}`)
+        e.preventDefault();
+    }
+
+    render() {
+        //const {username, topic, comments} = this.state
+        return(
+            <form>
+                <div>
+                    <label>Username: </label>
+                    <input type="text" value={this.state.username} onChange={this.handleUsernameChange}></input>
+                </div>
+                <div>
+                    <label>Comments: </label>
+                    <textarea type="text" value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+                </div>
+                <div>
+                   <label>Topic: </label>
+                   <select value={this.state.topic} onChange={this.handleTopicChange}>
+                    <option value="raect">React</option>
+                    <option value="angular">Angular</option>
+                    <option value="vue">Vue</option>
+                   </select>
+                </div>
+                <div>
+                    <button type="submit" onClick={this.handleSubmit}>Submit</button>
+                </div>
+            </form>
+        )
+    }
+}
+export default FormThis
